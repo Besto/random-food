86faed0f7a68dc727d73018ca8cdfe50502afc7d
Besto <besamato@gmail.com>
2017-08-21 23:28:59 +0700

    init proj

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,34 @@
 import { Component } from '@angular/core';
 
+declare var firebase:any
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'app';
+export class AppComponent {  
+  foodType : String = "carbo"
+  foodName : String = ""
+
+  constructor() {
+  }
+
+  ngOnInit(){
+  }
+
+  ngAfterViewInit(){
+  }
+
+  addFood(){
+    console.log(this.foodType+' '+this.foodName);
+    const foodRef = firebase.database().ref(this.foodType+'/');
+    const newRef = foodRef.push();
+      newRef.set({
+        name: this.foodName
+      });
+    alert('Your request has been sent.');
+    this.foodName = '';
+  }
+    
 }
